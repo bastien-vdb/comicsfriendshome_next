4e0ac6985b9707032f3fa60f487344c24b40472e
bastien-vdb <bastien.deboisrolin@gmail.com>
2022-12-21 11:07:15 +0100

    deletion of unused plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./node_modules/flowbite-react/**/*.js",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    "./public/**/*.html",
-    './src/**/*.{html,js}', './node_modules/tw-elements/dist/js/**/*.js',
   ],
   theme: {
     extend: {},
   },
-  plugins: [
-    require("flowbite/plugin"),
-    require('tw-elements/dist/plugin')
-  ],
+  plugins: [],
 }
