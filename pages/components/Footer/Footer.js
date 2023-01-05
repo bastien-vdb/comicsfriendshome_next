5c3f02689ebfd1fe82cf9ccdf83cc87d39fd2655
bastien-vdb <bastien.deboisrolin@gmail.com>
2023-01-05 23:18:46 +0100

    Sitemap.xml created + comicsfriends.io as main server + privacyAndPolicy page modified

--- pages/components/Footer/Footer.js
+++ pages/components/Footer/Footer.js
@@ -8,11 +8,10 @@ function Footer(props) {
             <div className='border-t border-gray-700 text-sm mt-10 p-2 flex flex-wrap justify-between items-center'>
                 <div className='flex flex-wrap justify-between items-center sm:gap-8'>
                     <Image alt='logo.png' src='/logo.png' className='m-2' height={500} width={100} />
-                    <p>All rights Reserved By <Link href="https://comicsfriends.art">ComicsFriends</Link></p>
+                    <p>All rights Reserved By <Link href="https://comicsfriends.io">ComicsFriends</Link></p>
                 </div>
 
                 <div className='flex flex-wrap justify-between items-center sm:gap-8 sm:m-2'>
-                    {/* <Link href='/components/Footer/TermsAndConditions'>Terms & Conditions</Link> */}
                     <Link href='/components/Footer/PrivacyAndPolicy'>Privacy & Policy</Link>
                 </div>
             </div>
